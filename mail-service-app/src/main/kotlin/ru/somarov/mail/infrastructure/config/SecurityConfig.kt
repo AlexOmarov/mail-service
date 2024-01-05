0d4ada57fc4e7b7c67b754a2a12b9fc079421473
Alex Omarov <shtil.a@yandex.ru>
2024-01-05 13:53:33 +0700

    MAIL-3 Added spring security basic auth
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

---
+++ mail-service-app/src/main/kotlin/ru/somarov/mail/infrastructure/config/SecurityConfig.kt
@@ -0,0 +1,36 @@
+package ru.somarov.mail.infrastructure.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService
+import org.springframework.security.core.userdetails.ReactiveUserDetailsService
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import java.security.SecureRandom
+
+@Configuration
+@EnableWebFluxSecurity
+private class SecurityConfig {
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder(ENCODER_STRENGTH, SecureRandom())
+    }
+
+    @Bean
+    fun userDetailsService(encoder: PasswordEncoder): ReactiveUserDetailsService {
+        val userDetails = User.builder()
+            .passwordEncoder { encoder.encode(it) }
+            .username("user")
+            .password("user")
+            .roles("USER")
+            .build()
+        return MapReactiveUserDetailsService(userDetails)
+    }
+
+    companion object {
+        const val ENCODER_STRENGTH = 11
+    }
+}
