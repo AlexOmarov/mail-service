b46cda8e6e1accf73cd3c37d0c290eaa501214d0
Alex Omarov <shtil.a@yandex.ru>
2024-06-14 20:03:23 +0700

    MAIL-21 Added telegram notification on pipeline completion, added clickhouse with kafka integration
    Signed-off-by: Alex Omarov <shtil.a@yandex.ru>

---
+++ config/analytics/clickhouse/init/init.sql
@@ -0,0 +1,16 @@
+  CREATE TABLE mail_service.queue (
+    id String,
+    status String
+  ) ENGINE = Kafka('kafka:9092', 'mail_service_mail_broadcast', 'clickhouse', 'JSONEachRow');
+
+CREATE TABLE mail_service.queue_dest (
+  created_timestamp Nullable(DateTime),
+  topic String,
+  partition String,
+  id String,
+  status String
+) ENGINE = MergeTree ORDER BY (status);
+
+CREATE MATERIALIZED VIEW mail_service.queue_mv TO mail_service.queue_dest AS
+SELECT *, _topic as topic, _partition as partition, _timestamp  as created_timestamp
+FROM mail_service.queue;
