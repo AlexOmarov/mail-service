ea0a583b7a91bbbe2d319856234206caf91dc89e
AlexOmarov <shtil.a@yandex.ru>
2024-08-29 19:02:17 +0700

    MAIL-28 Fixed clickhouse and postgres init scripts
    Signed-off-by: AlexOmarov <shtil.a@yandex.ru>

--- config/analytics/clickhouse/init/init.sql
+++ config/analytics/clickhouse/init/init.sql
@@ -1,9 +1,9 @@
-  CREATE TABLE mail_service.queue (
+  CREATE TABLE queue (
     id String,
     status String
   ) ENGINE = Kafka('persistence-kafka.persistence:9092', 'mail_service_mail_broadcast', 'clickhouse', 'JSONEachRow');
 
-CREATE TABLE mail_service.queue_dest (
+CREATE TABLE queue_dest (
   created_timestamp Nullable(DateTime),
   topic String,
   partition String,
@@ -11,6 +11,6 @@ CREATE TABLE mail_service.queue_dest (
   status String
 ) ENGINE = MergeTree ORDER BY (status);
 
-CREATE MATERIALIZED VIEW mail_service.queue_mv TO mail_service.queue_dest AS
+CREATE MATERIALIZED VIEW queue_mv TO queue_dest AS
 SELECT *, _topic as topic, _partition as partition, _timestamp  as created_timestamp
-FROM mail_service.queue;
+FROM queue;
